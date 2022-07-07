b306dcf61b8746903c5812b96a110837ec50ad68
moonral2022 <moonral2022@163.com>
2022-07-07 21:06:06 +0800

    init

---
+++ src/main/java/com/moonral/work/JmyMoonralWorkApplication.java
@@ -0,0 +1,13 @@
+package com.moonral.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmyMoonralWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmyMoonralWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moonral/work/JmyMoonralWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonral.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmyMoonralWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
